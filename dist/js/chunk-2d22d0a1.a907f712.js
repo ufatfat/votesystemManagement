4ca32a373c16854c3ccb96f06c16393cb17f921f
ufatfat <lzff@ufatfat.com>
2021-12-15 19:04:05 +0800

    almost done.

--- dist/js/chunk-2d22d0a1.a907f712.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d0a1"],{f694:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},c=[],s={name:"System"},u=s,r=t("2877"),d=Object(r["a"])(u,a,c,!1,null,"8fda704a",null);n["default"]=d.exports}}]);
-//# sourceMappingURL=chunk-2d22d0a1.a907f712.js.map
